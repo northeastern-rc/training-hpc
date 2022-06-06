a83b7010df3b3756a752a0d83087e6d7632338f7
Manasvita Joshi <github@manasvita.com>
2022-06-05 21:41:46 -0400

    Moved June 2021 training material on Parallel Programming to this folder

---
+++ 2021-06/Exercise_5/main_py.bash
@@ -0,0 +1,36 @@
+#!/bin/bash 
+#SBATCH --partition=express
+#SBATCH --job-name=MPIScaling
+#SBATCH --ntasks=128		#request 128 tasks that will allocate 1 cpu per task 
+#SBATCH --time=00:30:00
+#SBATCH --constraint=ib         # Run only on nodes that support IB
+
+######## change number of MPI tasks here: #################
+mpiNtasks=128
+##########################
+
+#Clean env of other conda modules or installations:
+conda deactivate
+module purge
+
+#Load the InfiniBand-supported MPI-library and the python environment to run MPI with python:
+module load discovery gcc/7.3.0 mpich/3.3.2-skylake-gcc7.3
+module load miniconda3/2020-11-skylake
+
+#Test number of MPI tasks between 1 and 128 (mult by 2):
+for (( nTasks=1; nTasks<=$mpiNtasks; nTasks=$nTasks*2 ))
+do
+
+	# check what time it is before the job starts
+	STARTT=`date +%s%N | cut -b1-13`
+	# run the parallel code "calcMatrix.py" using the openmpi libraries: 
+	mpirun -n $nTasks python calcMatrix.py
+
+	# check what time it is when the command finishes
+	ENDT=`date +%s%N | cut -b1-13`
+	ELAPSED=`echo 0 | awk -v e=$ENDT -v s=$STARTT '{print e-s}'`
+
+	echo "MPI-tasks: $nTasks , run-time: $ELAPSED ms" > performance.$nTasks.log
+done
+
+cat performance.* | awk '{print $2,$5}' | sort -nk1 > performance.all
